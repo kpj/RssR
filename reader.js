d318e81fbab9689d5e4f94faf5c0613f4089b6e4
kpj <kpjkpjkpjkpjkpjkpj@googlemail.com>
2011-11-21 20:46:57 +0100

    Style improvements

--- reader.js
+++ reader.js
@@ -4,6 +4,7 @@ var interDur = 100000
 var where = "re.py"
 var number = 40
 var isLoading = false
+var loadChanged = false
 var curFeed = ""
 
 function onerror(req, errmsg, err) {
@@ -14,10 +15,11 @@ function onerror(req, errmsg, err) {
 	else {
 		console.log("error: " + errmsg);
 	}
-	isLoading = false
+	setLoad(false)
 }
 
 function getSubs() {
+	setLoad(true)
 	$.ajax({
 		url: where,
 		dataType: 'json',
@@ -36,40 +38,44 @@ function getSubs() {
 }
 
 function getFeeds(url, num) {
-	isLoading = true
+	setLoad(true)
 	console.log("Asking for "+url)
 	$.ajax({
-    url: where,
-    dataType: 'json',
-    timeout: '5000',
-    cache: false,
-    data: {"data": url,
-					 "numb": num},
-    error: onerror,
-    success: function (response) { 
-      if (response != null) { 
-        console.log("Recieved feeds...")
-        feeds = response
+		url: where,
+		dataType: 'json',
+		timeout: '5000',
+		cache: false,
+		data: {"data": url,
+					"numb": num},
+		error: onerror,
+		success: function (response) { 
+			if (response != null) { 
+				console.log("Recieved feeds...")
+				feeds = response
 				genFeeds()
-      } 
-    } 
-  })
+			}
+		}
+	})
 }
 
 function genSubs() {
-  // Delete old entries
-  $('#subs').empty()
-  // Append new entries
-  for (p in subis) {
-    $('#subs').append(
-      $('<div>')
-        .text(subis[p]["title"])
-        .attr("data-xyz", subis[p]["id"])
-        .click(function () {
-          getFeeds("https://www.google.com/reader/api/0/stream/contents/" + $(this).attr("data-xyz"), number)
-        })
-    )
-  }
+	// Delete old entries
+	$('#subs').empty()
+	// Append new entries
+	for (var p in subis) {
+		$('#subs').append(
+			$('<div>')
+				.text(subis[p]["title"])
+				.attr("data-xyz", p)
+				.attr("id", "list_" + p)
+				.attr("class", "list")
+				.click(function (e) {
+					document.getElementById('list_' + $(this).attr("data-xyz")).style.backgroundColor = "#FF9933"
+					getFeeds("https://www.google.com/reader/api/0/stream/contents/" + subis[$(this).attr("data-xyz")]["id"], number)
+			})
+		)
+	}
+	setLoad(false)
 }
 
 function genFeeds() {
@@ -81,13 +87,16 @@ function genFeeds() {
   // Delete old entries
   $('#feeds').empty()
   // Append new entries
-  for (p in feeds["items"]) {
+  for (var p in feeds["items"]) {
 		if (feeds["items"][p]["content"] != undefined) {
 			$('#feeds').append(
 				$('<div>')
 					.text(feeds["items"][p]["title"])
 					.attr("data-xyz", p)
+					.attr("id", "list_" + p)
+      	  .attr("class", "list")
 					.click(function (e) {
+						document.getElementById('list_' + $(this).attr("data-xyz")).style.backgroundColor = "#FF9933"
 						handleContent(feeds["items"][$(this).attr("data-xyz")]["content"]["content"]) // <- The right way !
 					})
 			)
@@ -97,7 +106,10 @@ function genFeeds() {
       	$('<div>')
         	.text(feeds["items"][p]["title"])
 					.attr("data-xyz", p)
+          .attr("id", "list_" + p)
+          .attr("class", "list")
         	.click(function (e) {
+						document.getElementById('list_' + $(this).attr("data-xyz")).style.backgroundColor = "#FF9933"
 						handleContent(feeds["items"][$(this).attr("data-xyz")]["summary"]["content"])
 					})
     	)
@@ -107,13 +119,16 @@ function genFeeds() {
         $('<div>')
           .text(feeds["items"][p]["title"])
           .attr("data-xyz", p)
+          .attr("id", "list_" + p)
+          .attr("class", "list")
           .click(function (e) {
+						document.getElementById('list_' + $(this).attr("data-xyz")).style.backgroundColor = "#FF9933"
 						handleContent(feeds["items"][$(this).attr("data-xyz")]["title"])
 					})
       )
 		}
   }
-	isLoading = false
+	setLoad(false)
 }
 
 function handleContent(obj) {
@@ -124,19 +139,37 @@ function setNoti(str) {
 	document.getElementById("nots").innerHTML = str
 }
 
+function find(pat) {
+	for (var p in feeds["items"]) {
+		console.log(pat)
+		console.log(JSON.stringify(feeds["items"][p]).search(pat))
+	}
+}
+
 function tick() {
-  // Refresh whole data
-  getSubs()
+	// Refresh whole data
+	getSubs()
 }
 tick()
 var inter = setInterval(tick, interDur)
 
-function loads() {
-	if (isLoading) {
-		setNoti("Loading...")
+function setLoad(bol) {
+	if (bol) {
+		isLoading = true
 	}
 	else {
+		isLoading = false
+		loadChanged = false
+	}
+}
+
+function loads() {
+	if (isLoading && !loadChanged) {
+		setNoti("<progress>working...</progress>")
+		loadChanged = true
+	}
+	else if (!isLoading) {
 		setNoti("Idling")
 	}
 }
-var loading = setInterval(loads, 600)
+var loading = setInterval(loads, 300)
